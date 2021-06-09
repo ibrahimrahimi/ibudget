60fa07a820d9c4868c93cc51267a649d963ce71a
Ibrahim Rahimi <irahimi@netlinks.af>
2021-06-10 02:06:29 +0430

    Add basic HTML code.

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
+// BUDGET CONTROLLER 
 var budgetController = (function() {
-    var x = 26;
-    var add = function(a){
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
-
+    
 })();
 
+
+// UI CONTROLLER
 var UIController = (function() {
     // some code here
 })();
 
+
+// PUBLIC APPLICATION CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl) {
-    var z = budgetCtrl.publicTest(23);
-    return {
-        secondPublic: function(){
-            console.log(z);
-        }
-    }
+    
 })(budgetController, UIController);
+
+

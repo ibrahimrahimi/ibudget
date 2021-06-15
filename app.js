be3a33b3bac2352b6eba3717e5c4aecb2ae53afa
Ibrahim Rahimi <irahimi@netlinks.af>
2021-06-15 08:16:53 +0430

    Add get inputs value code.

--- app.js
+++ app.js
@@ -6,13 +6,34 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    // some code here
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value,
+                description : document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value,
+            }
+        }
+    }
 })();
 
 
 // PUBLIC APPLICATION CONTROLLER
 var controller = (function(budgetCtrl, uiCtrl) {
-    
+
+    var ctrlAddItem = function(){
+        var input = uiCtrl.getInput()
+        console.log(input);
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keycode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController, UIController);
 
 

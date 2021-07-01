5b6b7e7f607e95c3324182d697672a2467eae626
Ibrahim Rahimi <irahimi@netlinks.af>
2021-07-01 10:26:38 +0430

    Improve code to format numbers.

--- app.js
+++ app.js
@@ -130,7 +130,26 @@ var UIController = (function() {
         budgetPercentage: '.budget__expenses--percentage',
         container: '.container',
         expenseItemPercentage: '.item__percentage' 
-    }
+    };
+
+
+    var formatNumbers = function(num, type) {
+        var numSplit, int, deci;
+
+        num = Math.abs(num);
+        num = num.toFixed(2);
+
+        numSplit = num.split('.');
+        int = numSplit[0];
+
+        if(int.length > 3) {
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+        }
+
+        deci = numSplit[1];
+
+        return (type === 'expense' ? '-' : '+') + ' ' + int + '.' + deci;
+    };
 
     return {
         getInput: function() {
@@ -158,7 +177,7 @@ var UIController = (function() {
             
             html = html.replace('%id%', obj.id);
             newHtml = html.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%',formatNumbers(obj.value, type));
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
 
         },
